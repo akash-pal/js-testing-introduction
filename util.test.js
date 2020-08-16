d278e4cb4191bfd9647970cf64f1ae72e2c7c91e
Akash Pal <akash.pal@amdocs.com>
2020-08-16 16:27:12 +0530

    add end to end test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const {generateText, checkAndGenerate} =  require('./util');
 
 /* unit test*/
@@ -18,3 +19,20 @@ test('should generate a valid text output', () => {
     const text = checkAndGenerate('Akash', 27);
     expect(text).toBe('Akash (27 years old)');
 });
+
+/*end-to-end-test*/
+test('should create an element with text and correct class', async ()=> {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80
+    });
+    const page = await browser.newPage();
+    await page.goto('file:///C:/Users/akashpal/Projects/js-testing-introduction/index.html');
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
